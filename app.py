a608d60b422bf2d095181f9739a73ee01f0eb62c
Chris Kneller <christian.kneller@gmail.com>
2022-02-07 19:10:02 +0000

    Better naming

--- app.py
+++ app.py
@@ -1,22 +1,24 @@
 from flask import Flask, jsonify
 
+import database
 from models import Assay, Compound
-from database import connect_to_sqlite
+
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+mydb = database.DEFAULT_SQLITE_DB
 
 
 @app.route("/api/compounds")
 def api_compounds():
-    with connect_to_sqlite("compound_assay") as session:
+    with database.connect_to_sqlite(mydb) as session:
         compounds = session.query(Compound).all()
         return jsonify(compounds)
 
 
 @app.route("/api/compound/<compound_id>")
 def api_compound(compound_id: str):
-    with connect_to_sqlite("compound_assay") as session:
+    with database.connect_to_sqlite(mydb) as session:
         compound = (
             session.query(Compound)
             .filter(Compound.compound_id == compound_id)
@@ -27,14 +29,14 @@ def api_compound(compound_id: str):
 
 @app.route("/api/assays")
 def api_assays():
-    with connect_to_sqlite("compound_assay") as session:
+    with database.connect_to_sqlite(mydb) as session:
         assays = session.query(Assay).all()
         return jsonify(assays)
 
 
 @app.route("/api/assay/<result_id>")
 def api_assay(result_id: str):
-    with connect_to_sqlite("compound_assay") as session:
+    with database.connect_to_sqlite(mydb) as session:
         assay = (
             session.query(Assay)
             .filter(Assay.result_id == result_id)
