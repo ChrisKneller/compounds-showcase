b98da6157abd9e6d7fff2af07a5c11edd470fc05
Christian Kneller <christian.kneller@gmail.com>
2022-02-08 18:08:04 +0000

    Add test url resolving

--- dashapp.py
+++ dashapp.py
@@ -1,7 +1,9 @@
+from re import split
 from dash import Dash, html, dcc, Input, Output, callback
 import dash_bio as dashbio
 import plotly.express as px
 import pandas as pd
+import re
 import warnings
 
 import getter
@@ -29,7 +31,7 @@ fig = px.scatter(
 
 # Set up the page layout
 app.layout = html.Div(children=[
-    dcc.Location(id='url', refresh=False),  # represents the browser bar
+    dcc.Location(id="url", refresh=False),  # represents the browser bar
     html.H1(children="Compound explorer"),
     html.Div(id="page-content"),
     html.H2(children="Compounds summary"),
@@ -40,42 +42,60 @@ app.layout = html.Div(children=[
         id="example-graph",
         figure=fig
     ),
-    # html.Div([
-    #     dashbio.Jsme(
-    #         smiles=df["smiles"][0]
-    #     )
-    # ])
+
     html.Div(id="smiles-object"),
 
+    html.Div(id="url-resolver")
 
 ])
 
 
-@callback(Output('page-content', 'children'),
-          [Input('url', 'pathname')])
+@callback(Output("page-content", "children"),
+          [Input("url", "pathname")])
 def display_page(pathname):
     return html.Div([
-        html.H3(f'You are on page {pathname}')
+        html.H3(f"You are on page {pathname}")
     ])
 
-
-@callback(Output('smiles-object', 'children'),
-          [Input('url', 'pathname')])
-def display_smiles(pathname):
-    compound_id = pathname[1:]  # remove the leading / from pathname
-    try:
-        compound = df.loc[df["compound_id"] == int(compound_id)]
-    except:
-        return
-    # import pdb; pdb.set_trace()
-    smiles = compound["smiles"].values[0]
+def display_smiles(compound_id: str):
+    compound = getter.get_compound(compound_id)
+    smiles = compound["smiles"]
     return html.Div([
         dashbio.Jsme(
             smiles=smiles
         )
     ])
 
+def single_compound_page(compound_id: str):
+    pass
+
+def compounds_page():
+    pass
+
+def single_assay_page():
+    pass
+
+def assays_page():
+    pass
+
+@callback(Output("url-resolver", "children"),
+          [Input("url", "pathname")])
+def url_resolver(pathname):
+    # Match /{first}/{second}
+    matched_pattern = re.match(r"\/([a-zA-z]+)\/*(\d+)*", pathname)
+    first, second = matched_pattern.groups()
+    if first == "compounds":
+        if second:
+            return single_compound_page(second)
+        return compounds_page()
+    elif first == "assays":
+        if second:
+            return single_assay_page(second)
+        return assays_page()
+
+
+    return
+
 
 if __name__ == "__main__":
-    # import pdb; pdb.set_trace()
     app.run_server(debug=True)
