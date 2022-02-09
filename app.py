26f473e1aa99664c148f2a693478f58268b5c827
Chris Kneller <christian.kneller@gmail.com>
2022-02-09 21:57:36 +0000

    Format and add default host and port

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, jsonify
 
+import constants
 import database
 from models import Assay, Compound
 
 
 app = Flask(__name__)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
 mydb = database.DEFAULT_SQLITE_DB
 
 
@@ -37,14 +38,14 @@ def api_assays():
 @app.route("/api/assay/<result_id>")
 def api_assay(result_id: str):
     with database.connect_to_sqlite(mydb) as session:
-        assay = (
-            session.query(Assay)
-            .filter(Assay.result_id == result_id)
-            .one_or_none()
-        )
+        assay = session.query(Assay).filter(Assay.result_id == result_id).one_or_none()
         return jsonify(assay)
 
 
 @app.route("/")
 def hello_world():
     return "Hello, world"
+
+
+if __name__ == "__main__":
+    app.run(host=constants.HOST, port=constants.FLASK_PORT)
