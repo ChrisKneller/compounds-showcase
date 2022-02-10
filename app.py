95f3dc19817c1a541a6564bcb8e78099b1911833
Chris Kneller <christian.kneller@gmail.com>
2022-02-10 08:09:38 +0000

    Specific GET requests

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, jsonify
 
-import constants
 import database
 from models import Assay, Compound
 
@@ -10,14 +9,14 @@ app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
 mydb = database.DEFAULT_SQLITE_DB
 
 
-@app.route("/api/compounds")
+@app.route("/api/compounds", methods=["GET"])
 def api_compounds():
     with database.connect_to_sqlite(mydb) as session:
         compounds = session.query(Compound).all()
         return jsonify(compounds)
 
 
-@app.route("/api/compound/<compound_id>")
+@app.route("/api/compound/<compound_id>", methods=["GET"])
 def api_compound(compound_id: str):
     with database.connect_to_sqlite(mydb) as session:
         compound = (
@@ -28,14 +27,14 @@ def api_compound(compound_id: str):
         return jsonify(compound)
 
 
-@app.route("/api/assays")
+@app.route("/api/assays", methods=["GET"])
 def api_assays():
     with database.connect_to_sqlite(mydb) as session:
         assays = session.query(Assay).all()
         return jsonify(assays)
 
 
-@app.route("/api/assay/<result_id>")
+@app.route("/api/assay/<result_id>", methods=["GET"])
 def api_assay(result_id: str):
     with database.connect_to_sqlite(mydb) as session:
         assay = session.query(Assay).filter(Assay.result_id == result_id).one_or_none()
@@ -43,9 +42,13 @@ def api_assay(result_id: str):
 
 
 @app.route("/")
-def hello_world():
-    return "Hello, world"
+def links():
+    return (
+        "<a href='api/compounds'>Compounds</a>"
+        " | "
+        "<a href='api/assays'>Assays</a>"
+    )
 
 
 if __name__ == "__main__":
-    app.run(host=constants.HOST, port=constants.FLASK_PORT)
+    app.run()
