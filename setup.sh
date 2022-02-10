4990a76345e5cc4a08b9a3e8cf52ab0dbaa749f3
Chris Kneller <christian.kneller@gmail.com>
2022-02-10 14:50:53 +0000

    Add error checking in install script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,19 @@
 python3 -m venv .venv
 source .venv/bin/activate
+
+if [ $? -ne 0 ]; then
+    echo "Trying Windows setup..."
+    python -m venv .venv
+    .venv\\Scripts\\activate.bat
+    if [ $? -ne 0 ]; then
+        echo "Virtual environment couldn't be created. See errors above. Exiting."
+        exit $?
+    fi
+    pip3 install -r requirements.txt
+    python flaskapp/transform.py
+    python runservers.py
+fi
+
 pip3 install -r requirements.txt
 python3 flaskapp/transform.py
 python3 runservers.py
